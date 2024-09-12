c47af318f6c11dec5d02d3322f2328e3f07fb391
aleks1303 <aleksbulda13@gmail.com>
2024-09-12 21:53:41 +0300

    MySQL.sql-lesson2

--- lesson2.sql
+++ lesson2.sql
@@ -1,54 +1,54 @@
 use oleks;
 
-select distinct name, age from users; /*унікальні значення без дублікатів*/
+select distinct name, age from oleks.users; /*унікальні значення без дублікатів*/
 
 # використання декілька агрегатних функцій
-select avg(age), sum(age) from users;
+select avg(age), sum(age) from oleks.users;
 
 # теж саме але як в масиві
-select avg(age) from users
+select avg(age) from oleks.users
 union
-select sum(age) from users;
+select sum(age) from oleks.users;
 
 # математичні дії
-select id, price, price *2 as doublePrice from cars;
+select id, price, price *2 as doublePrice from oleks.cars;
 
 # текстове додавання з БД
-select concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) as srtings from users;
-select concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) as srtings from users;
+select concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) as srtings from oleks.users;
+select concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) as srtings from oleks.users;
 
 # знайти в БД по шаблону з фронтенду
-select * from users where concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) = 'My name is Max I`m 16 gender - male';
+select * from oleks.users where concat('My name is ', name, ' I`m ' ,age, ' gender - ', gender) = 'My name is Max I`m 16 gender - male';
 
 # пошук по алфавіту та кількості букв
-select * from users where name >= 'max';
+select * from oleks.users where name >= 'max';
 
-select * from users where age = id+17;
+select * from oleks.users where age = id+17;
 
 # нижній та верхній регістр
-select lower(name) from users;
-select upper(name) from users;
+select lower(name) from oleks.users;
+select upper(name) from oleks.users;
 
 
 # з'єднання таблиць (client, application, department)
-select * from application
-                  join client on client.idClient = application.Client_idClient
+select * from oleks.application
+join oleks.client on client.idClient = application.Client_idClient
 where FirstName='igor';
 
 # вибірка данних з однієї таблиці
-select application. * from application
-                               join client on client.idClient = application.Client_idClient
+select oleks.application. * from oleks.application
+join oleks.client on client.idClient = application.Client_idClient
 where FirstName='roman';
 
 # вибірка данних з іншої об'єднаної таблиці з додатковою інфо з іншої таблиці
-select client. *, application.CreditState from application
-                                                   join client on client.idClient = application.Client_idClient
+select oleks.client. *, oleks.application.CreditState from oleks.application
+join oleks.client on client.idClient = application.Client_idClient
 where FirstName='igor';
 
 # об'єднання трьох табличок
-select a. *, client.FirstName, City, d.DepartmentCity from client
-                                                               join oleks.application a on client.idClient = a.Client_idClient
-                                                               join oleks.department d on d.idDepartment = client.Department_idDepartment;
+select a. *, oleks.client.FirstName, City, d.DepartmentCity from oleks.client
+join oleks.application a on client.idClient = a.Client_idClient
+join oleks.department d on d.idDepartment = client.Department_idDepartment;
 
 
 # створення та об'єднання таблиць
