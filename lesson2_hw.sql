7e43ecadbd0c2b671d178561f924137b81d0b6b3
aleks1303 <aleksbulda13@gmail.com>
2024-09-16 13:00:41 +0300

    MySQL.sql-lesson2

--- lesson2_hw.sql
+++ lesson2_hw.sql
@@ -1,47 +1,47 @@
 -- 1.Вибрати усіх клієнтів, чиє ім'я має менше ніж 6 символів.
-    select * from client where length (FirstName) < 6;
+    select * from oleks.client where length (FirstName) < 6;
 
 -- 2.Вибрати львівські відділення банку.
-    select * from department where DepartmentCity = 'Lviv';
+    select * from oleks.department where DepartmentCity = 'Lviv';
 
 -- 3.Вибрати клієнтів з вищою освітою та посортувати по прізвищу.
-    select * from client where Education = 'high' order by LastName;
+    select * from oleks.client where Education = 'high' order by LastName;
 
 -- 4.Виконати сортування у зворотньому порядку над таблицею Заявка і вивести 5 останніх елементів.
-select * from application order by idApplication desc limit 5;
+select * from oleks.application order by idApplication desc limit 5;
 
 -- 5.Вивести усіх клієнтів, чиє прізвище закінчується на OV чи OVA.
-    select * from client where LastName like '%ov' or LastName like '%ova';
+    select * from oleks.client where LastName like '%ov' or LastName like '%ova';
 
 -- 6.Вивести клієнтів банку, які обслуговуються київськими відділеннями.
-select * from client
+select * from oleks.client
 join oleks.department d on d.idDepartment = client.Department_idDepartment
 where DepartmentCity = 'Kyiv';
 
 -- 7.Знайти унікальні імена клієнтів.
-select distinct FirstName from client;
+select distinct FirstName from oleks.client;
 
 -- 8.Вивести дані про клієнтів, які мають кредит більше ніж на 5000 гривень.
-select * from client
+select * from oleks.client
 join oleks.application a on client.idClient = a.Client_idClient
 where sum > 5000 and Currency = 'Gryvnia';
 
 -- 9.Порахувати кількість клієнтів усіх відділень та лише львівських відділень.
-select count(*) from client
+select count(*) from oleks.client
 join oleks.department d on d.idDepartment = client.Department_idDepartment;
 
-select count(*) from client
+select count(*) from oleks.client
 join oleks.department d on d.idDepartment = client.Department_idDepartment
 where DepartmentCity = 'Lviv';
 
 -- 10.Знайти кредити, які мають найбільшу суму для кожного клієнта окремо.
-select max(Sum), client.* from client
+select max(Sum), client.* from oleks.client
 join oleks.application a on client.idClient = a.Client_idClient
 group by idClient;
 
 
 -- 11. Визначити кількість заявок на крдеит для кожного клієнта.
-select count(*) from client
+select count(*) from oleks.client
 join oleks.application a on client.idClient = a.Client_idClient
  group by idClient;
 
@@ -51,17 +51,17 @@ join oleks.application a on client.idClient = a.Client_idClient
  group by idClient;*/
 
 -- 12. Визначити найбільший та найменший кредити.
-select max(Sum) from application;
-select min(Sum) from application;
+select max(Sum) from oleks.application;
+select min(Sum) from oleks.application;
 
 -- 13. Порахувати кількість кредитів для клієнтів,які мають вищу освіту.
-select count(*),idClient, FirstName, LastName, Education from client
-join oleks.application a on client.idClient = a.Client_idClient
+select count(*),idClient, FirstName, LastName, Education from oleks.client
+join oleks.application a on oleks.client.idClient = a.Client_idClient
 where Education = 'high'
 group by idClient;
 
 -- 14. Вивести дані про клієнта, в якого середня сума кредитів найвища.
-select avg(sum) as avg, client.* from client
+select avg(sum) as avg, client.* from oleks.client
 join oleks.application a on client.idClient = a.Client_idClient
 group by idClient
 order by avg desc
@@ -69,7 +69,7 @@ limit 1;
 
 -- 15. Вивести відділення, яке видало в кредити найбільше грошей
 select sum(Sum) as sum, department.DepartmentCity, department.idDepartment
-from department
+from oleks.department
 join oleks.client c on department.idDepartment = c.Department_idDepartment
 join oleks.application a on c.idClient = a.Client_idClient
 group by idDepartment
@@ -78,7 +78,7 @@ limit 1;
 
 
 -- 16. Вивести відділення, яке видало найбільший кредит.
-select max(Sum) as max_sum, department.* from department
+select max(Sum) as max_sum, department.* from oleks.department
 join oleks.client c on department.idDepartment = c.Department_idDepartment
 join oleks.application a on c.idClient = a.Client_idClient
 group by idDepartment
@@ -87,32 +87,32 @@ limit 1;
 
 
 -- 17. Усім клієнтам, які мають вищу освіту, встановити усі їхні кредити у розмірі 6000 грн.
-update application join oleks.client c on c.idClient = application.Client_idClient
+update oleks.application join oleks.client c on c.idClient = application.Client_idClient
 set Sum = 6000
 where Education = 'high';
 
 
 -- 18. Усіх клієнтів київських відділень пересилити до Києва.
-update client join oleks.department d on d.idDepartment = client.Department_idDepartment
+update oleks.client join oleks.department d on d.idDepartment = client.Department_idDepartment
 set City= 'Kyiv'
 where DepartmentCity = 'kyiv';
 
 
 -- 19. Видалити усі кредити, які є повернені.
-delete application.* from application
+delete application.* from oleks.application
 join oleks.client c on c.idClient = application.Client_idClient
 where CreditState = 'returned';
 
 
 -- 20. Видалити кредити клієнтів, в яких друга літера прізвища є голосною.
-   delete application.* from application
+   delete application.* from oleks.application
    join oleks.client c on c.idClient = application.Client_idClient
    where FirstName like
          '_a%' or FirstName like '_o%' or  FirstName like'_i%' or FirstName like '_y%';
 
 
 -- 21.Знайти львівські відділення, які видали кредитів на загальну суму більше ніж 5000
-select sum(Sum) as sum, department.DepartmentCity, department.idDepartment from department
+select sum(Sum) as sum, department.DepartmentCity, department.idDepartment from oleks.department
 join oleks.client c on department.idDepartment = c.Department_idDepartment
 join oleks.application a on c.idClient = a.Client_idClient
 where DepartmentCity = 'Lviv'
@@ -121,33 +121,33 @@ having sum(Sum) > 5000;
 
 
 -- 22.Знайти клієнтів, які повністю погасили кредити на суму більше ніж 5000
-select idClient, FirstName,LastName,CreditState, sum from client
-join application a on client.idClient = a.Client_idClient
+select idClient, FirstName,LastName,CreditState, sum from oleks.client
+join oleks.application a on client.idClient = a.Client_idClient
 where CreditState = 'Returned' and Sum > 5000;
 
 -- 23.Знайти максимальний неповернений кредит.
-select application.* from application
+select application.* from oleks.application
 where CreditState = 'Not returned'
 order by Sum desc
 limit 1;
 
 -- 24.Знайти клієнта, сума кредиту якого найменша
-select client.*, sum from client
+select client.*, sum from oleks.client
 join oleks.application a on client.idClient = a.Client_idClient
 order by sum
 limit 1;
 
 -- 25.Знайти кредити, сума яких більша за середнє значення усіх кредитів
-select * from application where Sum >
+select * from oleks.application where Sum >
 (select avg(Sum) from application);
 
 select *
-from application
+from oleks.application
 where Sum > (select avg(Sum) from application);
 -- 26. Знайти клієнтів, які є з того самого міста, що і клієнт, який взяв найбільшу кількість кредитів
 
 select *
-from client
+from oleks.client
 where City = (
     select c.City
     from client c
@@ -159,11 +159,11 @@ limit 1
 # select client.FirstName, client.LastName, client.idClient, application.
 
 select *
-from client
+from oleks.client
 where City = (
     select c.City
     from client c
-             join application a on c.idclient = a.client_idclient
+             join oleks.application a on c.idclient = a.client_idclient
     group by idclient
     order by count(*) desc
     limit 1
@@ -173,8 +173,8 @@ where City = (
 -- 27. Місто клієнта з найбільшою кількістю кредитів
 
 select c.City
-from client c
-         join application a on c.idclient = a.client_idclient
+from oleks.client c
+         join oleks.application a on c.idclient = a.client_idclient
 group by idclient
 order by count(*) desc
 limit 1;
@@ -184,6 +184,6 @@ limit 1;
 
 
 
-select * from client
+select * from oleks.client
                   join oleks.application a on client.idClient = a.Client_idClient
                   join oleks.department d on d.idDepartment = client.Department_idDepartment;
